969464f8ffc38c3bd4a61df8949a9206b3d9ba41
timotheegath <timothee.gathmann@ik.me>
2026-06-11 16:37:16 +0200

    finished freq penalty

--- custom_transformer.py
+++ custom_transformer.py
@@ -419,7 +419,7 @@ class TransformerSampler:
         while len(sequence) <= max_tokens_generated and (len(sequence) == 0 or sequence[-1] != self.tokenizer.eos_token_id):
             prompt_with_new_seq = prompt + "".join(sequence)
             input_tokens = Tensor(self.tokenizer.encode(prompt_with_new_seq)).to(t.int).to(device).unsqueeze(0)
-            logits:  Float[Tensor, "batch position d_vocab"] = self.model.forward(input_tokens) # For sampling, shouldn't we just keep the last position ? I will just keep the last position for now:
+            logits:  Float[Tensor, "batch position d_vocab"] = self.model.forward(input_tokens[:, -self.cfg.n_ctx :]) # For sampling, shouldn't we just keep the last position ? I will just keep the last position for now:
             last_logits: Float[Tensor, "batch d_vocab"] = logits[:, -1, :]
             next_token = self.sample_next_token(input_tokens.squeeze(0), last_logits.squeeze(0))
             sequence.append(str(self.tokenizer.decode(next_token))) # Add the new word to the sequence
